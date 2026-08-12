1819d696c83a0bdd7d6c21d18f156def4f4a06c0
Gabriel Garcia <contato.gabrielgarcia@gmail.com>
2026-08-12 21:41:16 +0100

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/fiap/techchallenge1restaurantsystem/TechChallenge1RestaurantSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge1restaurantsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallenge1RestaurantSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechChallenge1RestaurantSystemApplication.class, args);
+    }
+
+}
